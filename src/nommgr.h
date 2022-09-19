55778654536e90c2a1284c87e679e54f8272cca3
Carlos Aragonés <c.aragones@gmail.com>
2022-09-19 18:26:42 +0200

    removed include stdafx.h Minor fixes to be able to compile correctly compiling on MSVC v143, v142, v141, v140, clang-cl, MinGW 11.2, Linux GCC 11.2

--- src/nommgr.h
+++ src/nommgr.h
@@ -1,13 +1,15 @@
 // ---------------------------------------------------------------------------------------------------------------------------------
-//                                                 _     
-//                                                | |    
-//  _ __   ___  _ __ ___  _ __ ___   __ _ _ __    | |__  
-// | '_ \ / _ \| '_ ` _ \| '_ ` _ \ / _` | '__|   | '_ \ 
+/*
+//                                                 _
+//                                                | |
+//  _ __   ___  _ __ ___  _ __ ___   __ _ _ __    | |__
+// | '_ \ / _ \| '_ ` _ \| '_ ` _ \ / _` | '__|   | '_ \
 // | | | | (_) | | | | | | | | | | | (_| | |    _ | | | |
 // |_| |_|\___/|_| |_| |_|_| |_| |_|\__, |_|   (_)|_| |_|
-//                                   __/ |               
-//                                  |___/                
+//                                   __/ |
+//                                  |___/
 //
+*/
 // Memory manager & tracking software
 //
 // Best viewed with 8-character tabs and (at least) 132 columns
@@ -21,7 +23,7 @@
 //  * This software may not be re-distributed (in part or in whole) in a modified
 //    form without clear documentation on how to obtain a copy of the original work.
 //  * You may not use this software to directly or indirectly cause harm to others.
-//  * This software is provided as-is and without warrantee. Use at your own risk.
+//  * This software is provided as-is and without warranty. Use at your own risk.
 //
 // For more information, visit HTTP://www.FluidStudios.com
 //
@@ -31,28 +33,28 @@
 // Copyright 2000, Fluid Studios, Inc., all rights reserved.
 // ---------------------------------------------------------------------------------------------------------------------------------
 
-#ifdef	new
-#undef	new
+#ifdef  new
+#undef  new
 #endif
 
-#ifdef	delete
-#undef	delete
+#ifdef  delete
+#undef  delete
 #endif
 
-#ifdef	malloc
-#undef	malloc
+#ifdef  malloc
+#undef  malloc
 #endif
 
-#ifdef	calloc
-#undef	calloc
+#ifdef  calloc
+#undef  calloc
 #endif
 
-#ifdef	realloc
-#undef	realloc
+#ifdef  realloc
+#undef  realloc
 #endif
 
-#ifdef	free
-#undef	free
+#ifdef  free
+#undef  free
 #endif
 
 // ---------------------------------------------------------------------------------------------------------------------------------
